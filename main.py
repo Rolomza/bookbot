8a9ab6e337be9bb480ef931502d48333eb73df8b
Rolomza <augusto.robles.mza@gmail.com>
2023-12-30 11:55:47 -0300

    We built a function to filter and sort only alphabet letters and a function to print a report

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 def main():
     book_path = "books/frankestein.txt"
     book_text = get_book_text(book_path)
-    words = get_words_count(book_text)
-    print(f"Number of words: {len(words)}")
-    letters_count = get_chars(book_text)
-    print(f"Letters: {letters_count}")
+    word_count = get_words_count(book_text)
+    char_data = get_chars(book_text)
+    alphabet_data = filter_and_sort_alphabet_data(char_data)
+    print_report(book_path, word_count, alphabet_data)
 
 def get_book_text(path):
     with open(path) as file:
         return file.read()
 
 def get_words_count(text):
-    return text.split()
+    words = text.split()
+    return len(words)
 
 def get_chars(text):
     chars = {}
@@ -20,6 +21,33 @@ def get_chars(text):
             chars[letter] += 1
         else:
             chars[letter] = 1
-    return chars  
+    return chars
+
+def sort_on(d):
+    return d["num"]
+
+def filter_and_sort_alphabet_data(chars_dict):
+    chars_keys = list(chars_dict.keys())
+    alphabet_dict = {}
+
+    for char in chars_keys:
+        if char.isalpha():
+            alphabet_dict[char] = chars_dict[char]
+
+    sorted_alphabet_list = []
+
+    for ch in alphabet_dict:
+        sorted_alphabet_list.append({"char": ch, "num": alphabet_dict[ch]})
+
+    sorted_alphabet_list.sort(reverse=True, key=sort_on)
+    return sorted_alphabet_list
+
+def print_report(book_path, word_count, alphabet_data):
+    print(f"--- Report of {book_path} ---")
+    print(f"{word_count} words found in the document.")
+    print()
+    for item in alphabet_data:
+        print(f"The {item["char"]} letter was found {item["num"]} times")
+
 
 main()
